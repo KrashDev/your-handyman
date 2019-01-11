fbdf49754973bd62f5128f8a2ffe9c7c9ab775bb
donny <donny.gabai@gmail.com>
2019-01-10 21:56:29 -0500

    new logo and favicon

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
 $(document).ready(function() {
 
-  //google maps code
-  // Initialize and add the map
-  // function initMap() {
-  // The location of Uluru
-  // var uluru = {
-  //   lat: -84.308454,
-  //   lng: 33.860634
-  // };
-  // The map, centered at Uluru
-  // var map = new google.maps.Map(
-  //   document.getElementById('map'), {
-  //     zoom: 4,
-  //     center: uluru
-  //   });
-
-  // var APIKey = AIzaSyAJWV - SK85OFpSdJ9IkJewTyL_Ius8czFw;
-  // The marker, positioned at Uluru
-  //   var marker = new google.maps.Marker({
-  //     position: uluru,
-  //     map: map
-  //   });
-  // }
-  // <!--Load the API from the specified URL
-  // *
-  // The async attribute allows the browser to render the page
-  // while the API loads
-  //   *
-  //   The key parameter will contain your own API key(which is not needed
-  //     for this tutorial) *
-  //   The callback parameter executes the initMap()
-  //
-  // function
-  // -->
-  // <
-  // script async defer
-  // src = "https://maps.googleapis.com/maps/api/js?key=APIKey&callback=initMap" >
-
   //review function
   (function() {
     setInterval(function() {
@@ -75,13 +38,13 @@ $(document).ready(function() {
   });
 
   //navbar link animation 2
-  $('nav.navbar a').mouseover(function() {
+  $('.nav.navbar-nav a').mouseover(function() {
     $(this).css({
       "border-bottom": "2px solid #5d5dbb",
       "background-color": "#ffffff"
     });
   });
-  $('nav.navbar a').mouseout(function() {
+  $('.nav.navbar-nav a').mouseout(function() {
     $(this).css({
       "border-bottom": "2px solid #ffffff"
     });
